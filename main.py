af7c65dca3c9344c2d81ba716e2d912cbb8813d5
Ka13epka <138003067+Ka13epka@users.noreply.github.com>
2024-03-05 19:09:29 +0700

    3rd

--- main.py
+++ main.py
@@ -1,62 +1,59 @@
 import os
-import requests
 from urllib.parse import urlparse
 import argparse
+import requests
+from dotenv import load_dotenv
 
 
-
-def shorten_link(token, long_url):
+def shorten_link(headers, url):
     bit_url = 'https://api-ssl.bitly.com/v4/shorten'
     params = {
-        'long_url': long_url,
+        'long_url': url,
     }
-    headers = {
-        'Authorization': f'Bearer {token}',
-    }
-  
+
     response = requests.post(bit_url, headers=headers, json=params)
     response.raise_for_status()
-    return response.json()['link']
+    return response.json()['id']
 
 
-def count_clicks(token, bitlink):
-    bit_url = f'https://api-ssl.bitly.com/v4/bitlinks/{bitlink}/clicks/summary'
-    headers = {
-        'Authorization': f'Bearer {token}',
-    }
+def count_clicks(headers, bitlink):
+    url_sum = f'https://api-ssl.bitly.com/v4/bitlinks/{bitlink}/clicks/summary'
+
     params = {
         "unit": "month",
         "units": "-1"
     }
     
-    response = requests.get(bit_url, headers=headers, params=params)
+    response = requests.get(url_sum, headers=headers, params=params)
     response.raise_for_status()
     return response.json()["total_clicks"]
 
 
-def is_bitlink(token, url):
+def is_bitlink(headers, url):
     bit_url = f'https://api-ssl.bitly.com/v4/bitlinks/{url}'
-    headers = {
-        'Authorization': f'Bearer {token}',
-    }
 
     response = requests.get(bit_url, headers=headers)
     return response.ok
 
 
 def main():
-    parser = argparse.ArgumentParser(description='Автогенератор ссылок битли, и проверка сколько человек на неё зашли')
-    parser.add_argument('url', help='link')
+    load_dotenv()
+    apikey_bitly = os.getenv('APIKEY_BITLY')
+    headers = {
+        "Authorization": f"Bearer {apikey_bitly}",
+    }
+    parser = argparse.ArgumentParser(description='Сокращает ссылки и выводит количество переходов по ней')
+    parser.add_argument('link', help='Введите ссылку:')
     args = parser.parse_args()
-    url = args.url
-    token = os.getenv('APIKEY_BITLY')
-    parsed_url = urlparse(url)
-    combined_path = f'{parsed_url.netloc}{parsed_url.path}'
+    print(args.link)
+
+    parse_link = urlparse(args.link)
+    my_bitlink = f'{parse_link.netloc}{parse_link.path}'
     try:
-        if is_bitlink(token, combined_path):
-            print(count_clicks(token, combined_path))
+        if is_bitlink(headers, my_bitlink):
+            print(count_clicks(headers, my_bitlink))
         else:
-            print(shorten_link(token, url))    
+            print(shorten_link(headers, args.link))    
     except requests.exceptions.HTTPError as error:
         print(f'Неправильная ссылка: {error}')
     
